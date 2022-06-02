f14459fcf5d29b86a32b7bf892e1583cfba9ef44
Margaret Tanzosh <margarettanzosh@nestmk12.net>
2022-06-02 08:53:12 -0400

    Create gpa.c

---
+++ labs/gpa.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get the number of grades and validate input
+    int n;
+    do
+    {
+        n = get_int("Number of grades to average: ");
+    }
+    while (n <= 0);
+
+    // Declare a variable for sum and initialze to zero
+    float sum = 0;
+
+    // Ask for grades from the user and total them in sum
+    for (int i = 0; i < n; i++) {
+        float grade = get_float("Enter a grade: ");
+        sum += grade;
+    }
+
+    // Print out average GPA with one decimal point
+    printf("Your cumulative GPA is: %.1f\n", sum / n);
+}
